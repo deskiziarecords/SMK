a0547330bdf275d5906ba9145a1b3e588ca991db
J Roberto Jimenez <tijuanapaint@gmail.com>
2026-04-27 16:20:32 -0600

    Delete QUIMERIA-HYPERION-main directory

--- QUIMERIA-HYPERION-main/run.py
+++
@@ -1,7 +0,0 @@
-from SovereignMarketKernel import SovereignMarketKernel
-import pandas as pd
-
-kernel = SovereignMarketKernel()
-print("Sovereign Market Kernel is ready!")
-# df = pd.read_csv("your_data.csv", parse_dates=True, index_col=0)
-# kernel.on_new_bar(df)
